cbf0cdd76705f9dbb34f420af42e7177e53e25af
sgaunet <sgaunet@gmail.com>
2023-08-29 22:29:20 +0200

    Brew formula update for gtmpl version v1.0.2

--- Formula/gtmpl.rb
+++ Formula/gtmpl.rb
@@ -5,20 +5,20 @@
 class Gtmpl < Formula
   desc ""
   homepage "https://github.com/sgaunet/homebrew-tools"
-  version "1.0.1"
+  version "1.0.2"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.1/gtmpl_Darwin_x86_64.tar.gz"
-      sha256 "516f998f968336a9d526501c85a170486c93c658893f098f06f59ae596fede25"
+    if Hardware::CPU.arm?
+      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.2/gtmpl_Darwin_arm64.tar.gz"
+      sha256 "35d14d3d91c34497c106f5a95c8a03c636ef3deceb303319692ffceb1aa4e1cd"
 
       def install
         bin.install "gtmpl"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.1/gtmpl_Darwin_arm64.tar.gz"
-      sha256 "137319452aa5c4f5df3f289e7dad84c3714cdc13947982b761b695fbeda9e3b7"
+    if Hardware::CPU.intel?
+      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.2/gtmpl_Darwin_x86_64.tar.gz"
+      sha256 "ef33208f240c9a588d571fdac71201c094963446dcd43f1a7e0770c2665bb820"
 
       def install
         bin.install "gtmpl"
@@ -28,24 +28,24 @@ class Gtmpl < Formula
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.1/gtmpl_Linux_armv6.tar.gz"
-      sha256 "b2ffb064135ca701c13127defc3dc8f6931c9eb17e57a2a11f0ab68ef9784557"
+      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.2/gtmpl_Linux_armv6.tar.gz"
+      sha256 "c898faf7d375c8a89a2a5c24190472b1452d24908f4f38a59201130b38a8664d"
 
       def install
         bin.install "gtmpl"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.1/gtmpl_Linux_arm64.tar.gz"
-      sha256 "3653efb1444baf3a2f18df56b9248e27a074a0287882b6bb0cc52b5027a47953"
+      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.2/gtmpl_Linux_arm64.tar.gz"
+      sha256 "5fc5712b7b0e3f38e9318a0e6117aa5694e8a8b202db813e4927fa94c7b45da3"
 
       def install
         bin.install "gtmpl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.1/gtmpl_Linux_x86_64.tar.gz"
-      sha256 "7e1a74ed5eb577ef607b9ef53b65a957e9e1898cb715a78465ae669317939360"
+      url "https://github.com/sgaunet/gtmpl/releases/download/v1.0.2/gtmpl_Linux_x86_64.tar.gz"
+      sha256 "3c9258f45de53ecb68a5823c258712ad088d74813ff72856d955b4ecf80176bf"
 
       def install
         bin.install "gtmpl"
