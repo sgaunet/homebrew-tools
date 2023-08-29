43aec5b52aeb48263749e40c4fab31fadfc024f3
sgaunet <sgaunet@gmail.com>
2023-08-29 22:13:17 +0200

    Brew formula update for envtemplate version v0.2.1

--- Formula/envtemplate.rb
+++ Formula/envtemplate.rb
@@ -5,21 +5,21 @@
 class Envtemplate < Formula
   desc ""
   homepage "https://github.com/sgaunet/homebrew-tools"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.0/envtemplate_0.2.0_darwin_amd64"
-      sha256 "edf53e179ffa8cd7ebca2c3ec6deba439040da0dcb08d6349d8a5ed69cb62d4b"
+      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.1/envtemplate_0.2.1_darwin_amd64"
+      sha256 "7c8b01145360f0acc88f22884ca75ac86474a19ae6464a38fc2564b940d264f8"
 
       def install
         bin.install "envtemplate"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.0/envtemplate_0.2.0_darwin_arm64"
-      sha256 "e63b5b122fbd2fed95d53c37d5269b74a912d6a27e30c8c56f38148feb017f02"
+      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.1/envtemplate_0.2.1_darwin_arm64"
+      sha256 "299d209a868757a7d9191636d862ba6df97a4db4c244e1759f07841377c132aa"
 
       def install
         bin.install "envtemplate"
@@ -28,25 +28,25 @@ class Envtemplate < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.0/envtemplate_0.2.0_linux_arm64"
-      sha256 "1a55928ac3b629f118b3e23fa7d74474405f7a5f0bca55df30e897a742de07e9"
-
-      def install
-        bin.install "envtemplate"
-      end
-    end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.0/envtemplate_0.2.0_linux_armv6"
-      sha256 "97ea4a4ccac3fb3d9e42df6ca68afbb964da71a93d07a791474d740bdb57987a"
+      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.1/envtemplate_0.2.1_linux_armv6"
+      sha256 "5ee222458078cab625591a1627315479c67180d127926dcef3b867c03baf59ad"
 
       def install
         bin.install "envtemplate"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.0/envtemplate_0.2.0_linux_amd64"
-      sha256 "5cd0e4226259fb7df4cebca74d7b6568ce3bf20b441a23a42a01873e4dce0cc6"
+      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.1/envtemplate_0.2.1_linux_amd64"
+      sha256 "318dec87fa383a4d778d0875642e5a937a9ddbd220c338e7e0688845b967d533"
+
+      def install
+        bin.install "envtemplate"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/sgaunet/envtemplate/releases/download/v0.2.1/envtemplate_0.2.1_linux_arm64"
+      sha256 "382e7a7cb17c5387bc0ff772a42a3799d81813c47c456dd8ccec548c5d9d6c24"
 
       def install
         bin.install "envtemplate"
